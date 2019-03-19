e032a1d67bcdfe0c7e1289d0fcf7df3956f8f53c
John <connollyjohnf@gmail.com>
2019-03-19 03:00:04 +0000

    created questions.rb

---
+++ questions.rb
@@ -0,0 +1,11 @@
+class Question
+  attr_accessor :question, :answer
+
+  def initialize
+    num1 = rand(1...20)
+    num2 = rand(1...20)
+    @question = "What is #{num1} + #{num2}?"
+    @answer = num1 + num2
+  end
+
+end
