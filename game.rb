c4f72e4720cc666ff8a15ce81b421fe1bb121888
John <connollyjohnf@gmail.com>
2019-03-19 03:17:25 +0000

    allowed initialization to include the inputted usernames

--- game.rb
+++ game.rb
@@ -10,7 +10,11 @@ class Game
     puts 'Please enter your name, Player Two.'
     print '> '
     @player2 = Players.new(gets.chomp)
-    print 'Get ready to play MATH WARS in 3'
+    puts "Get ready, #{@player1.name} and #{@player2.name}."
+    sleep(1)
+    print "It's time to play MATH WARS in "
+    sleep(1)
+    print "3"
     sleep(1)
     print ', 2'
     sleep(1)
